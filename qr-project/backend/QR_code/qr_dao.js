5b31c2876aebb716feb404bdd59b1c7d38dc14a1
ansb <dksdudtjr1997@naver.com>
2024-08-05 14:49:01 +0900

    add qr-code

---
+++ qr-project/backend/QR_code/qr_dao.js
@@ -0,0 +1,82 @@
+import mysql2 from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+export class QR_DAO {
+    constructor(){
+        dotenv.config();
+        try{
+            this.pool = mysql2.createPool({
+                host: process.env.DATABASE_HOSTNAME,
+                user: process.env.DATABASE_USER,
+                password: process.env.DATABASE_PASSWORD,
+                database: process.env.DATABASE_SCHEMA,
+                port: process.env.DATABASE_PORT,
+                waitForConnections: true,
+                connectionLimit: 10,                    
+                idleTimeout: 60000,
+                queueLimit: 0,
+                enableKeepAlive: true,
+                keepAliveInitialDelay: 0
+            });
+            console.log("Connected to database pool");
+        }
+        catch(err){
+            console.err('database connection is problem: ' + err.stack);
+            throw err;
+        }
+    }
+
+    async transaction(sqls, values){
+        let conn = null;
+        try{
+            conn = await this.pool.getConnection();
+            await conn.beginTransaction();
+
+            let result, field = null;
+            for(const sql of sqls){
+                if(!values){
+                    [result, field] = await conn.execute(sql);
+                }
+                else{
+                    [result, field] = await conn.execute(sql, values);
+                }
+            }
+
+            await conn.commit();
+            return result;
+        }
+
+        catch(err){
+            if (!conn){
+                throw new Error("Connection is NULL");
+            }
+            await conn.rollback();
+            console.error("transaction error: "+ err.stack);
+            return null;
+        }
+
+        finally{
+            if (conn){
+                conn.release();
+            }
+        }
+    }
+
+    async create_qrcode(){ // crud
+        try{
+            let sql =
+            ""
+        }
+        catch (err){
+            console.error("database query [create_qrcode] is problem: "+ err.stack)
+        }
+    }
+
+    async read_qrcode(){
+
+    }
+
+    async delete_qrcode(){
+
+    }
+}
